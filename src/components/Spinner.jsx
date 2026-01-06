3e61fde58c8f45f35e1871824d9f041880074bf0
M Satish Kumar <msatish289kumar@gmail.com>
2026-01-06 19:02:52 +0530

    Backend Linked to App , Home screen - done

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center py-12">
+      <div className="h-12 w-12 animate-spin rounded-full border-4 border-sky-200 border-t-sky-600"></div>
+    </div>
+  );
+};
+
+export default Spinner;
