f71a17536a9a1ed08346aa5f0c4ea89bd1dc952f
Eswaraprabu <eswaraprabukumarasamy@gmail.com>
2026-01-17 13:32:40 +0530

    fixed deployment error

--- src/pages/AdminAddProduct.jsx
+++ src/pages/AdminAddProduct.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import edit_ic from "../assets/icons/edit_ic.png";
-import bin_ic from "../assets/icons/bin_ic.png";
+import edit_ic from "../assets/Icons/edit_ic.png";
+import bin_ic from "../assets/Icons/bin_ic.png";
 import {
   createProduct,
   uploadProductImage,
